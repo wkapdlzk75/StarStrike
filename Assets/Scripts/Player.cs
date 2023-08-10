9869da4e73cf928290ae25cc8852a338926f5fb9
Seo <wkapdlzk75@naver.com>
2023-08-10 15:43:15 +0900

    Player Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,14 @@ public class Player : MonoBehaviour
     public bool isTouchBottom;
     public bool isTouchRight;
     public bool isTouchLeft;
+
+    Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
         float moveHori = Input.GetAxisRaw("Horizontal");    // 좌우
@@ -21,17 +29,25 @@ public class Player : MonoBehaviour
             moveVert = 0;
 
         Vector3 curPos = transform.position;                // 현재위치 가져옴
-        Vector3 nextPos = new Vector3(moveHori, moveVert, 0) * speed * Time.deltaTime;
+        Vector3 nextPos = new Vector3(moveHori, moveVert, 0) * speed * Time.deltaTime;  // 새 위치값
 
         transform.position = curPos + nextPos;              // 이동 반영
+
+        // 좌우 이동 버튼을 눌렀을 때와 뗐을 때
+        if (Input.GetButtonDown("Horizontal") || 
+            Input.GetButtonUp("Horizontal"))
+        {   //에니메이터의 Input 값을 moveHori 로 설정한다
+            animator.SetInteger("Input", (int)moveHori);
+        }
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D _collision)
     {
         // 경계선 밖으로 나갈 경우
-        if (collision.transform.CompareTag("Border"))
+        if (_collision.transform.CompareTag("Border"))
         {
-            switch (collision.gameObject.name)
+            switch (_collision.gameObject.name)
             {
                 case "Top": isTouchTop = true; break;
                 case "Bottom": isTouchBottom = true; break;
@@ -41,12 +57,12 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D _collision)
     {
         // 경계선 안
-        if (collision.transform.CompareTag("Border"))
+        if (_collision.transform.CompareTag("Border"))
         {
-            switch (collision.gameObject.name)
+            switch (_collision.gameObject.name)
             {
                 case "Top": isTouchTop = false; break;
                 case "Bottom": isTouchBottom = false; break;
