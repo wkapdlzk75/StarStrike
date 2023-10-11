8b9664189516bc53f045c0f1a7dfbf71c4b4579e
Seo <wkapdlzk75@naver.com>
2023-10-11 21:58:08 +0900

    UI 231011

--- Assets/Scripts/Manager/UI/UIManagerLobbyScene.cs
+++ Assets/Scripts/Manager/UI/UIManagerLobbyScene.cs
@@ -31,11 +31,12 @@ public class UIManagerLobbyScene : UIManager
     void Start()
     {
         StartCoroutine(LoadData());
+        SetResolution();
     }
 
     IEnumerator LoadData()
     {
-        yield return new WaitUntil(()=> SaveLoadManager.loadEnd);
+        yield return new WaitUntil(() => SaveLoadManager.loadEnd);
         UIUpdateStage();
         UpdateGoods();
         UpdateHighScore();
@@ -69,4 +70,20 @@ public class UIManagerLobbyScene : UIManager
         stageText.text = CurrentStage.ToString();
     }
 
+    /// <summary>
+    /// 해상도 고정 함수
+    /// </summary>
+    public void SetResolution()
+    {
+        int setWidth = 900 * 9 / 16; // 화면 너비
+        int setHeight = 900; // 화면 높이
+
+        //해상도를 설정값에 따라 변경
+        //3번째 파라미터는 풀스크린 모드를 설정 > true : 풀스크린, false : 창모드
+        Screen.SetResolution(setWidth, setHeight, false);
+    }
 }
+
+
+
+
