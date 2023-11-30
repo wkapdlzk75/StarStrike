fb560c60579bdc18ec5f2d6b6c04fc638f7f2599
GoonChim <wkapdlzk75@naver.com>
2023-11-30 15:09:06 +0900

    Joystick Improvements 231130

--- Assets/Scripts/Fuction/Controller.cs
+++ Assets/Scripts/Fuction/Controller.cs
@@ -5,24 +5,18 @@ using UnityEngine;
 using UnityEngine.EventSystems; // 키보드, 마우스, 터치를 이벤트로 오브젝에 보낼 수 있는 기능을 지원
 
 // 인터페이스.   커서를 올려놓고 Ctrl + . 하면 구현해야 할 함수들을 자동으로 생성
-public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Controller : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
     public RectTransform lever;
-    RectTransform rectTransform;
+    public RectTransform joystick;
 
-    //[SerializeField, Range(10, 150)]
     public float leverRange;
 
     Vector2 inputDirection;
     bool isInput;
 
-    //public TPSCharacterController controller;
     public Player player;
-    void Awake()
-    {
-        rectTransform = GetComponent<RectTransform>();
-    }
 
     // 드래그 시작할 때
     public void OnBeginDrag(PointerEventData eventData)
@@ -48,37 +42,28 @@ public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 
     void ControlJoystickLever(PointerEventData eventData)
     {
-        var inputPos = eventData.position - rectTransform.anchoredPosition;
+        var inputPos = eventData.position - joystick.anchoredPosition;
         var inputVector = inputPos.magnitude < leverRange ? inputPos : inputPos.normalized * leverRange;
         lever.anchoredPosition = inputVector;
         inputDirection = inputVector / leverRange;
-        inputDirection.x = (float)Math.Round(inputDirection.x);
-        inputDirection.y = (float)Math.Round(inputDirection.y);
+        inputDirection.x = RoundTowardsZero(inputDirection.x);
+        inputDirection.y = RoundTowardsZero(inputDirection.y);
         //Debug.Log($"Input Direction: {inputDirection}");
     }
 
-    
-    void InputControlVector()
+    int RoundTowardsZero(float number)
     {
-        // 조이스틱 입력 벡터값을 캐릭터에 전달
-        //Debug.Log(inputDirection.x + " / " + inputDirection.y);
-        //controller.MoveHandheld(inputDirection);
-        player.MoveHandheld(inputDirection);
-        
+        if (number > 0)
+            return (int)Math.Ceiling(number);
+        else
+            return (int)Math.Floor(number);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (isInput)
         {
-            InputControlVector();
+            player.MoveHandheld(inputDirection);
         }
     }
 }
